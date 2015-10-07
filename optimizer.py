0ccf79da5883229aeef63026f27af1016f848a56
Ottofabius <ottofabius@gmail.com>
2015-10-07 19:56:09 +0200

    fix small bug

--- optimizer.py
+++ optimizer.py
@@ -38,9 +38,9 @@ class BaseOptimizer(object):
         batches = np.arange(0, data[0].shape[0], self.batch_size)
         lb = 0
         for j in xrange(len(batches) - 1):
-            start = time.time()
+            print "start", time.time()
             inp = [d[batches[j]:batches[j + 1]].toarray()
-            print "toarray time = ", time.time() - start
+            print "end", time.time()
                    if d.shape[0] > 0 else d for d in data]
             objectives = np.array(self.ascent(*inp))
             if np.isnan(objectives).any():
