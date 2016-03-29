56209de4d05baf0e992fc75c30474a43dc065b2f
Ottofabius <ottofabius@gmail.com>
2016-03-29 13:01:47 +0200

    generic vae model class for 1 and 2 layers

--- generic_vae.py
+++ generic_vae.py
@@ -11,42 +11,26 @@ import cPickle as pickle
 from scipy.sparse import csr_matrix, csc_matrix
 from scipy.special import gammaln
 
-
 def relu(x):
     return T.switch(x<0,0,x)
-        
-class topic_model_1layer:
-    def __init__(self, argdict):
 
-        self.dimZ = argdict['dimZ']
+class topic_model:
+    def __init__(self, argdict):
+        
         self.learning_rate = th.shared(argdict['learning_rate'])
         self.batch_size = argdict['batch_size']
-        sigmaInit = argdict['sigmaInit']
-        self.voc_size = argdict['voc_size']
         self.e = 1e-8
         self.rp = argdict['rp']
-        self.K = argdict['HUe1']
 
-        We1 = th.shared(np.random.normal(0,1./np.sqrt(self.voc_size),(argdict['HUe1'], self.voc_size)).astype(th.config.floatX), name = 'We1')
-        be1 = th.shared(np.random.normal(0,1,(argdict['HUe1'],1)).astype(th.config.floatX), name = 'be1', broadcastable=(False,True))
+        #structure
+        self.dimZ = argdict['dimZ']
+        self.HUe1 = argdict['HUe1']
+        self.HUe2 = argdict['HUe2']
+        self.HUd1 = argdict['HUd1']
+        self.HUd2 = argdict['HUd2']
+        self.voc_size = argdict['voc_size']
 
-        We_mu = th.shared(np.random.normal(0,1./np.sqrt(float(argdict['HUe1'])),(self.dimZ,argdict['HUe1'])).astype(th.config.floatX), name = 'We_mu')
-        be_mu = th.shared(np.random.normal(0,1,(self.dimZ,1)).astype(th.config.floatX), name = 'be_mu', broadcastable=(False,True))
-
-        We_var = th.shared(np.random.normal(0,1./np.sqrt(float(argdict['HUe1'])),(self.dimZ, argdict['HUe1'])).astype(th.config.floatX), name = 'We_var')
-        be_var = th.shared(np.random.normal(0,1,(self.dimZ,1)).astype(th.config.floatX), name = 'be_var', broadcastable=(False,True))
-
-        Wd1 = th.shared(np.random.normal(0,1./np.sqrt(self.dimZ),(argdict['HUd1'], self.dimZ)).astype(th.config.floatX), name = 'Wd1')
-        bd1 = th.shared(np.random.normal(0,1,(argdict['HUd1'],1)).astype(th.config.floatX), name = 'bd1', broadcastable=(False,True))
-
-        Wd2 = th.shared(np.random.normal(0,1/np.sqrt(float(argdict['HUd1'])),(self.voc_size, argdict['HUd1'])).astype(th.config.floatX), name = 'Wd2')
-        bd2 = th.shared(np.random.normal(0,1,(self.voc_size,1)).astype(th.config.floatX), name = 'bd2', broadcastable=(False,True))
-
-
-        self.params = OrderedDict([('We1', We1), ('be1', be1), ('We_mu', We_mu), ('be_mu', be_mu),  \
-            ('We_var', We_var), ('be_var', be_var), ('Wd1', Wd1), ('bd1', bd1), ('Wd2', Wd2), ('bd2', bd2)])
-
-        # Adam
+        #Adam
         self.b1 = 0.1
         self.b2 = 0.001
         self.m = OrderedDict()
@@ -54,6 +38,51 @@ class topic_model_1layer:
 
         self.KLD_free = argdict['KLD_free']
         self.KLD_burnin = argdict['KLD_burnin']
+
+        # --------------    Define model-specific dimensions    --------------------
+
+        if argdict['HUe2']==0:
+            We1 = th.shared(np.random.normal(0,1./np.sqrt(self.voc_size),(argdict['HUe1'], self.voc_size)).astype(th.config.floatX), name = 'We1')
+            be1 = th.shared(np.random.normal(0,1,(argdict['HUe1'],1)).astype(th.config.floatX), name = 'be1', broadcastable=(False,True))
+
+            H_e_last = argdict['HUe1']
+
+        elif argdict['HUe2']!=0:
+            We1 = th.shared(np.random.normal(0,1./np.sqrt(argdict['voc_size']),(argdict['HUe1'], argdict['voc_size'])).astype(th.config.floatX), name = 'We1')
+            be1 = th.shared(np.random.normal(0,1,(argdict['HUe1'],1)).astype(th.config.floatX), name = 'be1', broadcastable=(False,True))
+
+            We2 = th.shared(np.random.normal(0,1./np.sqrt(float(argdict['HUe1'])),(argdict['HUe2'], argdict['HUe1'])).astype(th.config.floatX), name = 'We2')
+            be2 = th.shared(np.random.normal(0,1,(argdict['HUe2'],1)).astype(th.config.floatX), name = 'be2', broadcastable=(False,True))
+
+            H_e_last = argdict['HUe2']
+
+        We_mu = th.shared(np.random.normal(0,1./np.sqrt(float(H_e_last)),(self.dimZ,H_e_last)).astype(th.config.floatX), name = 'We_mu')
+        be_mu = th.shared(np.random.normal(0,1,(self.dimZ,1)).astype(th.config.floatX), name = 'be_mu', broadcastable=(False,True))
+
+        We_var = th.shared(np.random.normal(0,1./np.sqrt(float(H_e_last)),(self.dimZ, H_e_last)).astype(th.config.floatX), name = 'We_var')
+        be_var = th.shared(np.random.normal(0,1,(self.dimZ,1)).astype(th.config.floatX), name = 'be_var', broadcastable=(False,True))
+
+        Wd1 = th.shared(np.random.normal(0,1./np.sqrt(self.dimZ),(argdict['HUd1'], self.dimZ)).astype(th.config.floatX), name = 'Wd1')
+        bd1 = th.shared(np.random.normal(0,1,(argdict['HUd1'],1)).astype(th.config.floatX), name = 'bd1', broadcastable=(False,True))
+        
+        if argdict['HUd2']==0:
+            H_d_first = self.voc_size
+
+        elif argdict['HUd2']!=0:
+            H_d_first = argdict['HUd2']
+
+            Wd3 = th.shared(np.random.normal(0,1./np.sqrt(float(argdict['HUd2'])),(argdict['voc_size'], argdict['HUd2'])).astype(th.config.floatX), name = 'Wd3')
+            bd3 = th.shared(np.random.normal(0,1,(argdict['voc_size'],1)).astype(th.config.floatX), name = 'bd3', broadcastable=(False,True))
+
+        Wd2 = th.shared(np.random.normal(0,1/np.sqrt(float(argdict['HUd1'])),(H_d_first, argdict['HUd1'])).astype(th.config.floatX), name = 'Wd2')
+        bd2 = th.shared(np.random.normal(0,1,(H_d_first,1)).astype(th.config.floatX), name = 'bd2', broadcastable=(False,True))
+
+        self.params = dict([('We1', We1), ('be1', be1), ('We_mu', We_mu), ('be_mu', be_mu),  \
+            ('We_var', We_var), ('be_var', be_var), ('Wd1', Wd1), ('bd1', bd1), ('Wd2', Wd2), ('bd2', bd2)])
+        if self.HUe2!=0:
+            self.params.update(dict([('We2', We2), ('be2', be2)]))
+        if self.HUd2!=0:
+            self.params.update(dict([('Wd3', Wd3), ('bd3', bd3)]))
 
 
 
@@ -83,29 +112,33 @@ class topic_model_1layer:
 
         H = relu(H_lin)
 
+        if self.HUe2!=0:
+            H2_lin = T.dot(self.params['We2'], H) + self.params['be2']
+            H = relu(H2_lin)
+
         mu  = T.dot(self.params['We_mu'], H)  + self.params['be_mu']
         logvar = T.dot(self.params['We_var'], H) + self.params['be_var']
-
 
         eps = srng.normal((self.dimZ, self.batch_size), avg=0.0, std=1.0, dtype=theano.config.floatX)
         z = mu + T.exp(0.5*logvar)*eps
 
         H_d = relu(T.dot(self.params['Wd1'], z)  + self.params['bd1'])
 
-        y_notnorm = T.nnet.sigmoid(T.dot(self.params['Wd2'], H_d)  + self.params['bd2'])
+        if self.HUd2==0:
+            y_notnorm = T.nnet.sigmoid(T.dot(self.params['Wd2'], H_d)  + self.params['bd2'])
+        elif self.HUd2!=0:
+            H_d = relu(T.dot(self.params['Wd2'], H_d)  + self.params['bd2'])
+            y_notnorm = T.nnet.sigmoid(T.dot(self.params['Wd3'], H_d)  + self.params['bd3'])
+
         y = y_notnorm/T.sum(y_notnorm, axis=0)
 
         KLD_factor = T.minimum(1,T.maximum(0, (epoch - self.KLD_free)/self.KLD_burnin))
         KLD      =  -T.sum(T.sum(1 + logvar - mu**2 - T.exp(logvar), axis=0)/theano.sparse.basic.sp_sum(x, axis=0))
         KLD_train = KLD*KLD_factor
 
-        recon_err =  T.sum(theano.sparse.basic.sp_sum(x*T.log(y), axis=0)/theano.sparse.basic.sp_sum(x, axis=0))
+        recon_err =  T.sum(theano.sparse.basic.sp_sum(x*T.log(y+1e-20), axis=0)/theano.sparse.basic.sp_sum(x, axis=0))
         # recon_err =  T.sum(theano.sparse.sp_sum(theano.sparse.basic.mul(x, T.log(y)), axis=0)/theano.sparse.basic.sp_sum(x, axis=0))
 
-        # logx = theano.sparse.structured_log(x)
-        # loglogy = T.log(-T.log(y))
-        # xlogy = T.exp(theano.sparse.basic.add(logx, -loglogy))
-        # recon_err =  T.sum(T.sum(xlogy, axis=0)/theano.sparse.basic.sp_sum(x, axis=0))
 
         lowerbound_train = recon_err - KLD_train
         lowerbound = recon_err - KLD
@@ -120,18 +153,10 @@ class topic_model_1layer:
 
         gamma = T.sqrt(1 - (1 - self.b2) ** epoch)/(1 - (1 - self.b1)**epoch)
 
-        i=0
-        # Adam
         for parameter, gradient, m, v in zip(self.params.values(), gradients, self.m.values(), self.v.values()):
-            i+=1
+
             new_m = self.b1 * gradient + (1 - self.b1) * m
             new_v = self.b2 * (gradient**2) + (1 - self.b2) * v
-            
-            # if i%2 == 0:
-            #     updates[parameter] = parameter + self.learning_rate * gamma * new_m / (T.sqrt(new_v) + 1e-20) 
-            # else:
-            #     parameter_norm = parameter / T.sqrt(T.sum(T.sqr(parameter), axis=1, keepdims=True))
-            #     updates[parameter] = parameter_norm + self.learning_rate * gamma * new_m / (T.sqrt(new_v) + 1e-20)
 
             updates[parameter] = parameter + self.learning_rate * gamma * new_m / (T.sqrt(new_v) + 1e-20)
 
@@ -163,6 +188,13 @@ class topic_model_1layer:
 
         H[H<0] = 0
 
+        if self.HUe2!=0:
+            We2 = self.params["We2"].get_value() 
+            be2 = self.params["be2"].get_value() 
+
+            H =  np.dot(We2, H) + be2
+            H[H<0] = 0
+
         mu  = np.dot(We_mu, H)  + be_mu
         logvar = np.dot(We_var, H) + be_var
 
@@ -175,7 +207,7 @@ class topic_model_1layer:
         bd1 = self.params["bd1"].get_value()
 
         Wd2 = self.params["Wd2"].get_value()
-        bd2 = self.params["bd2"].get_value()
+        bd2 = self.params["bd2"].get_value()           
 
         z = np.random.normal(mu, np.exp(logvar))
 
@@ -183,7 +215,19 @@ class topic_model_1layer:
 
         H_d[H_d<0] = 0
 
-        y_lin = np.dot(Wd2, H_d)  + bd2
+        if self.HUd2==0:
+            y_lin = np.dot(Wd2, H_d)  + bd2
+
+        elif self.HUd2!=0:
+            Wd3 = self.params["Wd3"].get_value()
+            bd3 = self.params["bd3"].get_value() 
+
+            H_d = np.dot(Wd2, H_d) + bd2
+            H_d[H_d<0] = 0
+
+            y_lin = np.dot(Wd3, H_d)  + bd3
+
+        
         y_notnorm = 1./(1.+np.exp(-y_lin))
 
 
@@ -237,7 +281,7 @@ class topic_model_1layer:
         batches = np.arange(0,N,self.batch_size)
         if batches[-1] != N:
             batches = np.append(batches,N)
-        rest_batch = np.zeros((self.batch_size, self.K))
+        rest_batch = np.zeros((self.batch_size, self.HUe1)) #K=HUe1 for now
         for i in xrange(0,len(batches)-2):
             
 
@@ -251,9 +295,6 @@ class topic_model_1layer:
             recon_err += recon_err_batch
             KLD += KLD_batch
             KLD_train += KLD_train_batch
-            # if progress != int(50.*i/len(data_x)):
-            #     print '='*int(50.*i/len(data_x))+'>'
-            #     progress = int(50.*i/len(data_x))
 
         return lowerbound, recon_err, KLD, KLD_train
 
@@ -267,7 +308,7 @@ class topic_model_1layer:
         if batches[-1] != N:
             batches = np.append(batches,N)
 
-        rest_batch = np.zeros((self.batch_size, self.K))
+        rest_batch = np.zeros((self.batch_size, self.HUe1))
         for i in xrange(0,len(batches)-1):
             if batches[i+1]<N:
                 X_batch = data[batches[i]:batches[i+1]]
---
+++ train_vae_gen.py
@@ -0,0 +1,121 @@
+import numpy as np
+from helpfuncs import *
+from analysis import plot_stats, plot_used_dims
+import time
+from scipy.sparse import csr_matrix, csc_matrix
+import gzip
+import cPickle as pickle
+import scipy.sparse as sp
+from sklearn.utils import shuffle
+import sys
+import matplotlib.pyplot as plt
+from generic_vae import topic_model
+
+
+if __name__=="__main__":
+
+    # THEANO_FLAGS=optimizer=None
+
+    # import warnings
+    # warnings.filterwarnings("ignore")
+
+    argdict = parse_config(sys.argv[1])
+
+    x = load_dataset(argdict)
+
+    x_csc = csc_matrix(x)
+
+    # TODO put code below in load_dataset, include train/test splits
+    if argdict['rp']==1:        
+        print 'using random projection of rest in encoder'
+        f = gzip.open('data/'+argdict['dataset']+'/data_proj_'+str(argdict['minfreq'])+'.pklz','rb')
+        rest = pickle.load(f)
+        f.close()
+    
+
+    n_total, empty = x_csc.shape
+    n_test = argdict['testset_size']
+    n_train = argdict['trainset_size']
+
+    x_train = x_csc[:n_train,:]
+    x_test = x_csc[n_total-1-n_test:n_total,:] 
+    if argdict['rp']==1:
+        rest_train = rest[:n_train,:]
+        rest_test = rest[n_total-1-n_test:n_total,:]
+    else:
+        rest_train = None
+        rest_test = None
+
+    argdict['voc_size'] = x_train.shape[1]
+    print 'voc size:', argdict['voc_size'], "n_total:", n_total, "n_train:", n_train, "n_test:", n_test
+
+    used_features = load_used_features(argdict)
+
+    model = topic_model(argdict)
+
+    if len(sys.argv) > 2 and sys.argv[2] == "--load":
+        print "loading params for restart"
+        load_parameters(model, 'results/vae_own/' + sys.argv[1])
+        lowerbound_list, testlowerbound_list, KLD_list, KLD_used_list, recon_train_list, \
+                    recon_test_list, perplexity_list, perp_sem_list, epoch = load_stats('results/vae_own/' + sys.argv[1])
+        print "Restarting at epoch: " + str(epoch) + ' with lowerbounds ', lowerbound_list[-1], testlowerbound_list[-1]
+    else:
+        lowerbound_list, testlowerbound_list, KLD_list, KLD_used_list, recon_train_list, \
+                    recon_test_list, perplexity_list, perp_sem_list = ([] for i in range(8))
+        epoch = 0
+
+        print "estimating perplexity on test set with", argdict['samples'], "samples"
+        perplexity, perp_sem = perplexity_during_train(model, x_test, argdict, rest=rest_test)
+        perplexity_list = np.append(perplexity_list, perplexity)
+        perp_sem_list = np.append(perp_sem_list, perp_sem)
+        print "perplexity =", perplexity, 'with', perp_sem, 'sem'
+
+
+
+    print 'iterating' 
+    idx = np.arange(n_train)
+    while epoch < argdict['max_epochs']:
+
+        epoch += 1      
+        if epoch % argdict['save_every'] == 0:    
+
+            print "saving stats, params at epoch", epoch
+            # save_stats(            'results/vae_own/'+sys.argv[1], lowerbound_list, testlowerbound_list, KLD_list, KLD_used_list, \
+            #                                                         recon_train_list, recon_test_list, perplexity_list, perp_sem_list)
+            # save_parameters(model, 'results/vae_own/'+sys.argv[1])
+
+            print "estimating perplexity on test set with", argdict['samples'], "samples"
+            perplexity, perp_sem = perplexity_during_train(model, x_test, argdict, rest=rest_test)
+            perplexity_list = np.append(perplexity_list, perplexity)
+            perp_sem_list = np.append(perp_sem_list, perp_sem)
+            print "perplexity =", perplexity, 'with', perp_sem, 'sem'
+
+        start = time.time()  
+        
+        np.random.shuffle(idx)
+        x_train = x_train[idx,:]
+        if argdict['rp']==1:
+            rest_train = rest_train[idx,:]
+
+        lowerbound, recon_train, KLD, KLD_used = model.iterate(x_train, epoch, rest=rest_train)
+        testlowerbound, recon_test = model.getLowerBound(x_test, epoch, rest=rest_test)
+
+        if epoch == 1:
+            print time.time() - start, 'seconds for first epoch'
+        print 'epoch ', epoch, 'lb: ', lowerbound/n_train, 'lb test', testlowerbound/(n_test-n_test%argdict['batch_size']), 'recon test', recon_test/(n_test-n_test%argdict['batch_size'])
+
+        lowerbound_list     = np.append(lowerbound_list     , lowerbound    /n_train)
+        KLD_list            = np.append(KLD_list            , KLD           /n_train)
+        KLD_used_list       = np.append(KLD_used_list       , KLD_used      /n_train)
+        recon_train_list    = np.append(recon_train_list    , recon_train   /n_train)
+
+        recon_test_list     = np.append(recon_test_list     , recon_test    /(n_test-n_test%argdict['batch_size']))
+        testlowerbound_list = np.append(testlowerbound_list , testlowerbound/(n_test-n_test%argdict['batch_size']))
+ 
+    print "done, saving stats, params"
+    # save_stats(            'results/vae_own/'+sys.argv[1], lowerbound_list, testlowerbound_list, KLD_list, KLD_used_list, \
+    #                                                                 recon_train_list, recon_test_list, perplexity_list, perp_sem_list)
+    # save_parameters(model, 'results/vae_own/'+sys.argv[1])
+
+plot_stats(lowerbound_list, testlowerbound_list, KLD_list, KLD_used_list, perplexity_list, perp_sem_list, sys.argv[1], argdict['save_every'])
+plot_used_dims(model, x_test, sys.argv[1]) 
